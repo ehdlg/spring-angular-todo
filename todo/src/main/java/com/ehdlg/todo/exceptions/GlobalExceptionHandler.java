4b605460a893a1390800b6d0ee8ca7a913889291
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-02 19:56:01 +0200

    feat(validation): add exception handler for validation errors

--- todo/src/main/java/com/ehdlg/todo/exceptions/GlobalExceptionHandler.java
+++ todo/src/main/java/com/ehdlg/todo/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.ehdlg.todo.exceptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -24,6 +28,20 @@ public class GlobalExceptionHandler {
 
     return new ResponseEntity<ErrorResponse>(error, HttpStatus.CONFLICT);
 
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponse> handleValidationExceptions(
+      MethodArgumentNotValidException ex) {
+    List<String> errors = new ArrayList<>();
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+      String errorMessage = error.getDefaultMessage();
+      errors.add(errorMessage);
+    });
+
+    ErrorResponse errorResponse = new ErrorResponse(errors);
+
+    return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
   }
 
 }
