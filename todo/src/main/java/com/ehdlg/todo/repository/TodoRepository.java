8a48a929c428a9dbe2affb33602b6a3576bb5428
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-02 08:42:11 +0200

    feat(repository): add findByTitle, findByIsCompleted methods

--- todo/src/main/java/com/ehdlg/todo/repository/TodoRepository.java
+++ todo/src/main/java/com/ehdlg/todo/repository/TodoRepository.java
@@ -3,6 +3,11 @@ package com.ehdlg.todo.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.ehdlg.todo.model.Todo;
+import java.util.List;
 
 public interface TodoRepository extends CrudRepository<Todo, Long> {
+  public Todo findByTitle(String title);
+
+  public List<Todo> findByIsCompleted(Boolean isCompleted);
+
 }
