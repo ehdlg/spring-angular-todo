c6a06665d69f029a2df402fd8b377da685190142
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-02 19:55:04 +0200

    feat(exceptions): add errors property

--- todo/src/main/java/com/ehdlg/todo/exceptions/ErrorResponse.java
+++ todo/src/main/java/com/ehdlg/todo/exceptions/ErrorResponse.java
@@ -1,17 +1,35 @@
 package com.ehdlg.todo.exceptions;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
   private String error;
+  private List<String> errors;
 
   public ErrorResponse(String error) {
     this.error = error;
+  }
+
+  public ErrorResponse(List<String> errors) {
+    this.errors = errors;
   }
 
   public String getError() {
     return error;
   }
 
+  public List<String> getErrors() {
+    return errors;
+  }
+
   public void setError(String error) {
     this.error = error;
+  }
+
+  public void setErrors(List<String> errors) {
+    this.errors = errors;
   }
 }
