0040b3b06073090b1c7a7a9f2bb4f81c2cd36dc3
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-03 11:50:13 +0200

    feat: implement components, move AppComponent to component folder

---
+++ todo-ui/src/app/components/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TodoService } from '../services/todo.service';
+import { TodoComponent } from './todo.component';
+import { TodoType } from '../../types';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, TodoComponent],
+  template: `
+    <main class="max-w-[800px] mx-auto mt-20">
+      <h1 class="text-4xl m-4">TODO</h1>
+      <app-todo />
+    </main>
+
+    <router-outlet />
+  `,
+})
+export class AppComponent {}
