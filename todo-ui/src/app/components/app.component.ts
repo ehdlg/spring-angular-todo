717c86d2942cded3313c5dbf6b164b0c93360067
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-04 09:38:33 +0200

    feat(styles): update styles

--- todo-ui/src/app/components/app.component.ts
+++ todo-ui/src/app/components/app.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs';
-import { TodoService } from '../services/todo.service';
 import { TodoComponent } from './todo.component';
-import { TodoType } from '../../types';
+import { AddTodoComponent } from './add-todo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TodoComponent],
+  imports: [RouterOutlet, TodoComponent, AddTodoComponent],
   template: `
-    <main class="max-w-[800px] mx-auto mt-20 p-4">
+    <main class="max-w-[800px] mx-auto mt-20 p-4 flex flex-col gap-6">
       <h1 class="text-4xl m-4">TODO</h1>
+      <app-add-todo />
       <app-todo />
     </main>
 
--- todo-ui/src/app/components/todo-item.component.ts
+++ todo-ui/src/app/components/todo-item.component.ts
@@ -6,11 +6,15 @@ import { TodoType } from '../../types';
   standalone: true,
   imports: [],
   template: `
-    <li class="border-b border-slate-200 p-4 last:border-none list-disc">
-      {{ todo.title }}
+    <li class="border-b border-slate-200 p-4 flex gap-4">
+      <input
+        type="checkbox"
+        [id]="todo.id"
+        class="w-5 h-5 cursor-pointer appearance-none border border-gray-300  rounded-md mr-2 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100"
+      />
+      <label [htmlFor]="todo.id">{{ todo.title }}</label>
     </li>
   `,
-  styles: ``,
 })
 export class TodoItemComponent {
   @Input({ required: true }) todo!: TodoType;
