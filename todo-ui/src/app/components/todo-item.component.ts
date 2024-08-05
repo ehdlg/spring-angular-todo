70bb16e17bbf3e38505146f2a5d934aa569c3d5d
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-05 14:01:14 +0200

    feat(component): add update todo status funtion

--- todo-ui/src/app/components/todo-item.component.ts
+++ todo-ui/src/app/components/todo-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TodoType } from '../../types';
+import { TodoService } from '../services/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -8,9 +9,11 @@ import { TodoType } from '../../types';
   template: `
     <li class="border-b border-slate-200 p-4 flex gap-4">
       <input
+        (click)="updateStatus(todo)"
         type="checkbox"
         [id]="todo.id"
         class="w-5 h-5 cursor-pointer appearance-none border border-gray-300  rounded-md mr-2 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100"
+        [checked]="todo.isCompleted"
       />
       <label [htmlFor]="todo.id">{{ todo.title }}</label>
     </li>
@@ -18,4 +21,13 @@ import { TodoType } from '../../types';
 })
 export class TodoItemComponent {
   @Input({ required: true }) todo!: TodoType;
+  constructor(private service: TodoService) {}
+
+  updateStatus(todo: TodoType) {
+    const updatedTodo: TodoType = { ...todo, isCompleted: !todo.isCompleted };
+
+    this.service.update(updatedTodo).subscribe();
+
+    this.service.loadTodos();
+  }
 }
