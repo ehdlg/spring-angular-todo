601a2b7387fd84914770404d5a360439bfe3e611
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-05 09:17:26 +0200

    feat(interceptor): implement error handler interceptor logic

--- todo-ui/src/app/interceptors/error-handler.interceptor.ts
+++ todo-ui/src/app/interceptors/error-handler.interceptor.ts
@@ -1,5 +1,19 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { ErrorReponse } from '../../types';
 
 export const errorHandlerInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  return next(req).pipe(
+    catchError((error: ErrorReponse) => {
+      let errorMessage = 'An error occured';
+
+      if (error.status === 400) {
+        errorMessage = (error.error.errors as string[]).join('|');
+      } else {
+        errorMessage = error.error.error as string;
+      }
+
+      return throwError(() => new Error(errorMessage));
+    })
+  );
 };
