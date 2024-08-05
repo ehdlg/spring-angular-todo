8f838b1ce6d4fe5d8f46c4d924480953c2b10874
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-05 10:40:12 +0200

    feat(component): implement ErrorComponent

--- todo-ui/src/app/components/app.component.ts
+++ todo-ui/src/app/components/app.component.ts
@@ -2,15 +2,24 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from './todo.component';
 import { AddTodoComponent } from './add-todo.component';
+import { AsyncPipe } from '@angular/common';
+import { ErrorComponent } from './error.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TodoComponent, AddTodoComponent],
+  imports: [
+    RouterOutlet,
+    TodoComponent,
+    AddTodoComponent,
+    AsyncPipe,
+    ErrorComponent,
+  ],
   template: `
     <main class="max-w-[800px] mx-auto mt-20 p-4 flex flex-col gap-6">
       <h1 class="text-4xl m-4">TODO</h1>
       <app-add-todo />
+      <app-error />
       <app-todo />
     </main>
 
