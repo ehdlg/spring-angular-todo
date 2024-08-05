3da3735f0226e42d4e0bafdc49089c50507ceef5
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-05 09:18:15 +0200

    feat(types): add ErrorResponse type

--- todo-ui/src/types.ts
+++ todo-ui/src/types.ts
@@ -1,5 +1,14 @@
+import { HttpErrorResponse } from '@angular/common/http';
+
 export type TodoType = {
   id: number;
   title: string;
   isCompleted: boolean;
 };
+
+export interface ErrorReponse extends HttpErrorResponse {
+  error: {
+    error?: string;
+    errors?: string[];
+  };
+}
