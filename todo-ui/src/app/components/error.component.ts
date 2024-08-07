380dc5dd000b946a8da26948723680c1fd13fc5a
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-07 12:41:26 +0200

    feat(component): add svg icon to close error component button

--- todo-ui/src/app/components/error.component.ts
+++ todo-ui/src/app/components/error.component.ts
@@ -15,9 +15,22 @@ import { ErrorHandlerService } from '../services/error-handler.service';
       <button
         type="button"
         (click)="clearErrors()"
-        class="absolute top-1 right-5 text-xl"
+        class="absolute top-1 right-5"
       >
-        X
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6 text-slate-800"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6 18 18 6M6 6l12 12"
+          />
+        </svg>
       </button>
       <h3 class="text-red-500 text-xl font-bold">Errors</h3>
       <ul>
